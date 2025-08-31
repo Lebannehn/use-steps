a98ffc5328b2f17ff3f88f5540a78341bc46ce6f
Evgenii Astapushkin <lebannehn.karnimirie@gmail.com>
2025-08-31 23:47:08 +0200

    some logic

--- package/index.ts
+++ package/index.ts
@@ -38,15 +38,24 @@ const useStage = <T>({ stageList = [], mode = STAGE_MODE.SEQUENTIAL }: Props<T>)
 	}, [currentStageIndex]);
 
 	const _canProceed = useCallback(
-		() => {
-			console.log('checking can proceed');
-			if (availableStages.length === 1 || currentStageIndex === availableStages.length - 1) {
+		(isForwardDirection = true) => {
+			if (availableStages.length === 1) {
 				console.log('checking can proceed - no');
 				return false;
-			} else {
-				console.log('checking can proceed - yes');
-				return true;
 			}
+
+			if (mode === STAGE_MODE.SEQUENTIAL) {
+				if (isForwardDirection) {
+					console.log('checking can proceed - no');
+					return currentStageIndex !== availableStages.length - 1;
+				} else {
+					console.log('checking can proceed - no');
+					return currentStageIndex !== 0;
+				}
+			}
+			console.log('checking can proceed - yes');
+
+			return true;
 		},
 		[availableStages, currentStageIndex]
 	);
@@ -55,7 +64,7 @@ const useStage = <T>({ stageList = [], mode = STAGE_MODE.SEQUENTIAL }: Props<T>)
 		console.log('next stage');
 		_checkStageListSanity();
 		console.log('next stage - ok');
-		if (_canProceed()) {
+		if (_canProceed(true)) {
 			setCurrentStageIndex(currentStageIndex + 1);
 		}
 	};
@@ -63,7 +72,7 @@ const useStage = <T>({ stageList = [], mode = STAGE_MODE.SEQUENTIAL }: Props<T>)
 		console.log('previous stage');
 		_checkStageListSanity();
 		console.log('previous stage - ok');
-		if (_canProceed()) {
+		if (_canProceed(false)) {
 			setCurrentStageIndex(currentStageIndex - 1);
 		}
 	};
